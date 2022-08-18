784c30707ec8c29767e8690758e15736c0fca493
AmacEroglu <amacceroglu@gmail.com>
2022-08-19 00:08:15 +0300

    renkler eklendi.

--- VkiApp/Program.cs
+++ VkiApp/Program.cs
@@ -3,12 +3,14 @@
 public class Program
 {
 
-    struct VKI
+    public struct VKI
     {
         public double patientHeight;
         public double patientWeight;
         public double bodyIndex;
         public string diagnosis;
+        public string hastaninİsmi;
+       
 
         public  double PatientResult()
         {
@@ -17,11 +19,14 @@ public class Program
         
         public void WriteScreen()
         {
+
+            Console.WriteLine($"Hastanın İsmi: {hastaninİsmi}");
             Console.WriteLine($"Patient Height: {patientHeight}");
             Console.WriteLine($"Patient Weight: {patientWeight}");
             Console.WriteLine($"Diagnosis: {diagnosis}");
             Console.WriteLine($"Body Index: {PatientResult()}");
         }
+
 
         public void BodyIndex()
         {
@@ -30,13 +35,13 @@ public class Program
             {
                 diagnosis = "Zayıf";
             }
-            if (bodyIndex >18 && bodyIndex < 24)
+            if (bodyIndex >18 && bodyIndex < 25)
             {
                 diagnosis = "İdeal";
             }
             if (bodyIndex > 25 && bodyIndex < 30)
             {
-                diagnosis = "Hafif kilolu";
+                diagnosis = "Hafif Kilolu";
             }
             if (bodyIndex > 30 )
             {
@@ -48,6 +53,8 @@ public class Program
     }
 
 
+    static List<VKI> VKIlist = new List<VKI>();
+
     static void Main()
     {
         Menu();
@@ -56,23 +63,67 @@ public class Program
      static void Menu()
     {
         Console.Clear();
+        Console.WriteLine("1.Yeni Hasta\n2. Hasta Listesi\n3. Çıkış");
+        MenuSelection();
+    }
 
-        Console.WriteLine("Lütfen hasta bilgilerini girin.");
-        NewVKI();
+    static void MenuSelection()
+    {
+        Console.Write("Yapılacak işlemi seçin : ");
+        string choose = Console.ReadLine();
+        switch (choose)
+        {
+            case "1":
+                NewVKI();
+                break;
+            case "2":
+                ListOfPatient();
+                break;
+            case "3":
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Hatalı seçim yaptınız. Lütfen tekrar değer girin.");
+                MenuSelection();
+                break;
+               
+        }
+    }
+
+    private static void ListOfPatient()
+    {
+        Console.Clear();
+
+        foreach (var patient in VKIlist)
+        {
+            ConsoleColour(patient.diagnosis);
+            patient.WriteScreen();
+            ChangeColour();
+        }
+        Again();
     }
 
     static void NewVKI()
     {
         VKI x = new VKI();
+        x.diagnosis = "";
 
+        Console.Write("Hastanın İsmi: ");
+        x.hastaninİsmi = Console.ReadLine();
         Console.Write("Hastanın boyu: ");
         x.patientHeight = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine(x.patientHeight);
         Console.Write("Hastanın kilosu: ");
         x.patientWeight = Convert.ToDouble(Console.ReadLine());
 
+        Console.Clear();
+
         x.BodyIndex();
+        ConsoleColour(x.diagnosis);
         x.WriteScreen();
+        ChangeColour();
+
+        VKIlist.Add(x);
         Again();
     }
 
@@ -89,6 +140,41 @@ public class Program
             Console.WriteLine("Lütfen doğru bir değer giriniz");
             Again();
         }
+    }
+
+        
+      static void ConsoleColour(string diagnosis)
+    {
+        switch (diagnosis)
+        {
+            case "Zayıf":
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case "İdeal":
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+            case "Hafif Kilolu":
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                break;
+            case "Obez":
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    static void ChangeColour()
+    {
+        Console.ResetColor();
+    }
+
+    static void ReturnToMenu()
+    {
+        Console.WriteLine("Menüye dönmek için Enter'a basın");
+        Console.ReadLine();
+        Menu();
     }
 
 
